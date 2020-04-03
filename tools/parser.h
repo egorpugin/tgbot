3f9c5a3900dcc7a103d25f9f890ebe789301aff4
Egor Pugin <egor.pugin@gmail.com>
2020-04-03 19:01:38 +0300

    [gen] Move types in their own header.

--- tools/parser.h
+++ tools/parser.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "types.h"
+
 #include <libxml/HTMLparser.h>
 #include <nlohmann/json.hpp>
 #include <primitives/emitter.h>
@@ -7,42 +9,6 @@
 #include <primitives/http.h>
 #include <primitives/templates.h>
 #include <primitives/xml.h>
-
-struct Emitter;
-
-struct Field
-{
-    String name;
-    std::vector<String> types;
-    String description;
-    bool optional = false;
-    int array = 0;
-
-    void save(nlohmann::json &j) const;
-    void emitField(primitives::CppEmitter &ctx) const;
-    void emitFieldType(primitives::CppEmitter &ctx) const;
-};
-
-struct Type
-{
-    String name;
-    Field return_type;
-    std::vector<Field> fields;
-    std::vector<String> oneof;
-    String description;
-
-    void save(nlohmann::json &j) const;
-
-    bool is_type() const { return isupper(name[0]); }
-    bool is_oneof() const { return !oneof.empty(); }
-
-    void emitType(primitives::CppEmitter &ctx) const;
-    void emitCreateType(primitives::CppEmitter &ctx) const;
-    void emitMethod(const Emitter &e, primitives::CppEmitter &h, primitives::CppEmitter &cpp) const;
-    void emitMethodRequestType(primitives::CppEmitter &ctx) const;
-    void emitFwdDecl(primitives::CppEmitter &ctx) const;
-    void emitProtobuf(primitives::CppEmitter &ctx) const;
-};
 
 struct Parser
 {
