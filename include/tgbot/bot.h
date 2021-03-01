6d5b955f1a54eb9a27a1b9fe899836261a20e40f
Egor Pugin <egor.pugin@gmail.com>
2021-03-02 02:52:14 +0300

    Move types into bot.h

--- include/tgbot/bot.h
+++ include/tgbot/bot.h
@@ -1,10 +1,36 @@
 #pragma once
 
-#include "types.h"
-
+#include <cstdint>
+#include <memory>
+#include <optional>
 #include <span>
+#include <string>
+#include <variant>
+#include <vector>
 
 namespace tgbot {
+
+using Boolean = bool;
+using Integer = std::int64_t;
+using Float = double;
+using String = std::string;
+
+template <class T>
+using Optional = std::optional<T>;
+template <class ... Args>
+using Ptr = std::unique_ptr<Args...>;
+template <class ... Args>
+using Variant = std::variant<Args...>;
+template <class ... Args>
+using Vector = std::vector<Args...>;
+
+template <class ... PtrArgs, class ... Args>
+auto createPtr(Args && ... args) {
+    return std::make_unique<PtrArgs...>(args...);
+}
+
+#define TGBOT_TYPE_API TGBOT_API
+#include <types.inl.h>
 
 namespace detail {
 
