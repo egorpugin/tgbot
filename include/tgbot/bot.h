a83f308803253812bae78637e58ad11b28d18a25
Egor Pugin <egor.pugin@gmail.com>
2021-03-02 01:04:47 +0300

    Misc.

--- include/tgbot/bot.h
+++ include/tgbot/bot.h
@@ -9,7 +9,7 @@ struct curl_http_client;
 /// This object holds other objects specific for this bot instance.
 struct TGBOT_API bot {
     bot(const std::string &token);
-    ~bot();
+    ~bot() = default;
 
     /// returns token for accessing api
     const std::string &token() const { return token_; }
@@ -22,14 +22,14 @@ struct TGBOT_API bot {
 
     const std::string &base_url() const { return base_url_; }
     const std::string &base_file_url() const { return base_file_url_; }
-    std::string make_file_url(const std::string &file_path) const;
+    std::string make_file_url(const std::string &file_path) const { return base_file_url_ + token_ + "/" + file_path; }
 
 private:
     std::string token_;
     std::unique_ptr<curl_http_client> http_client_;
     struct api api_;
-    std::string base_url_;
-    std::string base_file_url_;
+    std::string base_url_{ "https://api.telegram.org/bot" };
+    std::string base_file_url_{ "https://api.telegram.org/file/bot" };
 };
 
 }
--- src/bot.cpp
+++ src/bot.cpp
@@ -2,9 +2,6 @@
 
 #include <tgbot/curl_http_client.h>
 
-#include <memory>
-#include <string>
-
 namespace tgbot {
 
 bot::bot(const std::string &token)
@@ -12,15 +9,6 @@ bot::bot(const std::string &token)
     , http_client_(std::make_unique<curl_http_client>())
     , api_(*this)
 {
-    base_url_ = "https://api.telegram.org/bot";
-    base_file_url_ = "https://api.telegram.org/file/bot";
-}
-
-bot::~bot() {
-}
-
-std::string bot::make_file_url(const std::string &file_path) const {
-    return base_file_url_ + token_ + "/" + file_path;
 }
 
 }
