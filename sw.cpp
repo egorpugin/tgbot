919722abcee13e186f729567378336f885f12b12
Egor Pugin <egor.pugin@gmail.com>
2021-04-26 19:06:13 +0300

    Bump sw version.

--- sw.cpp
+++ sw.cpp
@@ -1,6 +1,6 @@
 void build(Solution &s)
 {
-    auto &tgbot = s.addLibrary("tgbot", "1.1.1");
+    auto &tgbot = s.addLibrary("tgbot", "1.1.2");
     tgbot += Git("https://github.com/egorpugin/tgbot");
 
     auto &apitool = tgbot.addExecutable("apitool");
