775704e599db98c6a656651b0ed1aac568c6a03b
Egor Pugin <egor.pugin@gmail.com>
2021-12-07 16:36:18 +0300

    Update Bot API to 5.5.

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@ OLD_VERSION=$1
 NEW_VERSION=$2
 
 curl https://core.telegram.org/bots/api > TelegramBotAPI.html
+sed -i '$ d' TelegramBotAPI.html # remove the last line with generation time
 sed -i -e "s/$OLD_VERSION/$NEW_VERSION/g" README.md sw.cpp
 git commit -am "Update Bot API to $NEW_VERSION."
 git tag -a $SW_VERSION.$NEW_VERSION -m "$SW_VERSION.$NEW_VERSION"
-git push --all
+git push --tags
 sw build && sw upload org.sw.demo
