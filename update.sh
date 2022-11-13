56d6a5f731fc5a889de7769102f2fe2145a738f3
Egor Pugin <egor.pugin@gmail.com>
2022-11-13 13:51:18 +0300

    Fix readme version change. Automatically create gh releases.

--- update.sh
+++ update.sh
@@ -14,12 +14,14 @@ fi
 git pull origin master
 curl https://core.telegram.org/bots/api > TelegramBotAPI.html
 $SED -i -e '$ d' TelegramBotAPI.html # remove the last line with generation time
-$SED -i -e "s/$OLD_VERSION/$NEW_SW_VERSION/g" README.md sw.cpp
+$SED -i -e "s/$OLD_VERSION/$NEW_VERSION/g" README.md
+$SED -i -e "s/$OLD_VERSION/$NEW_SW_VERSION/g" sw.cpp
 die() { echo "$*" 1>&2 ; exit 1; }
 sw build $* || die "Build failed"
 git commit -am "Update Bot API to $NEW_VERSION."
 git tag -a $NEW_SW_VERSION -m "$NEW_SW_VERSION"
 git push
 git push --tags
+gh api --method POST /repos/egorpugin/tgbot/releases -f tag_name="$NEW_SW_VERSION" -f name="$NEW_SW_VERSION" -f body="Update to Bot API v$NEW_VERSION." > /dev/null
 # why build? delete?
 sw build $* && sw upload org.sw.demo
