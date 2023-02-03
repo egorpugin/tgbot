f20f41d1f966306fa860b7be07ef559a28fe9801
Egor Pugin <egor.pugin@gmail.com>
2023-02-03 22:18:19 +0300

    Update Bot API to 6.5.

--- sw.cpp
+++ sw.cpp
@@ -1,6 +1,6 @@
 void build(Solution &s)
 {
-    auto &tgbot = s.addLibrary("tgbot", "1.1.5.6.4");
+    auto &tgbot = s.addLibrary("tgbot", "1.1.5.6.5");
     tgbot += Git("https://github.com/egorpugin/tgbot");
 
     auto &apitool = tgbot.addExecutable("apitool");
