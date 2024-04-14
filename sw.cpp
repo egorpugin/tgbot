fdd445ad5097e58d735c1c76443e49b9bd4a6273
Egor Pugin <egor.pugin@gmail.com>
2024-04-14 15:29:48 +0300

    Fix variants. Bump version.

--- sw.cpp
+++ sw.cpp
@@ -1,6 +1,6 @@
 void build(Solution &s)
 {
-    auto &tgbot = s.addLibrary("tgbot", "1.1.5.7.2");
+    auto &tgbot = s.addLibrary("tgbot", "1.1.6.7.2");
     tgbot += Git("https://github.com/egorpugin/tgbot");
 
     auto &apitool = tgbot.addExecutable("apitool");
