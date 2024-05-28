555f918675a86270d70cf053bd50e68ee4e145c8
Egor Pugin <egor.pugin@gmail.com>
2024-05-28 15:20:49 +0300

    Update Bot API to 7.4.

--- sw.cpp
+++ sw.cpp
@@ -21,7 +21,7 @@
 
 void build(Solution &s)
 {
-    auto &tgbot = s.addLibrary("tgbot", "1.1.7.7.3");
+    auto &tgbot = s.addLibrary("tgbot", "1.1.7.7.4");
     tgbot += Git("https://github.com/egorpugin/tgbot");
 
     auto &apitool = tgbot.addExecutable("apitool");
