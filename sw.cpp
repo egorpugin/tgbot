fb0aa36b334db2c7194c6ca8be5996def73b0f9e
Egor Pugin <egor.pugin@gmail.com>
2026-02-10 03:26:59 +0300

    Fix extra stdc++ dependency.

--- sw.cpp
+++ sw.cpp
@@ -13,7 +13,7 @@
         bot += #n ".cpp"; \
         if (bot.getCompilerType() == CompilerType::MSVC) \
             bot.CompileOptions.push_back("/bigobj"); \
-        if (bot.getCompilerType() == CompilerType::GNU) \
+        else \
             bot += "stdc++exp"_slib; \
         bot += "org.sw.demo.tgbot.curl_skeleton"_dep __VA_OPT__(,) __VA_ARGS__ ; \
     }
