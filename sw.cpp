99e041220c0400573cdfe202a3b3f0475523ef90
Egor Pugin <egor.pugin@gmail.com>
2026-07-14 15:35:35 +0300

    Update Bot API to 10.2.

--- sw.cpp
+++ sw.cpp
@@ -25,7 +25,7 @@
 
 void build(Solution &s)
 {
-    auto &tgbot = s.addLibrary("tgbot", "1.1.11.10.1");
+    auto &tgbot = s.addLibrary("tgbot", "1.1.11.10.2");
     tgbot += Git("https://github.com/egorpugin/tgbot");
 
     auto cppstd = cpp26;
